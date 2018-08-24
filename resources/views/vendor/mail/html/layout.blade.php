c35df1dc69e6759408934c659ebe0e626011a578
Emil Kristensen <eKristensen@users.noreply.github.com>
2018-08-24 20:19:48 +0200

    Schema.org fix

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    {{ $oneclickaction ?? '' }}
 </head>
 <body>
-    {{ $oneclickaction ?? '' }}
     {{ $preheader ?? '' }}
     <style>
         @media only screen and (max-width: 600px) {
