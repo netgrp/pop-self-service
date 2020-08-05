31d1043faaf0a6e9f60ccf3a1d0482ae13273ca3
Emil Kristensen <eKristensen@users.noreply.github.com>
2020-08-05 08:49:18 +0200

    Copyright on text only mail fixed
    Signed-off-by: Emil Kristensen <eKristensen@users.noreply.github.com>

--- resources/views/vendor/mail/text/message.blade.php
+++ resources/views/vendor/mail/text/message.blade.php
@@ -21,7 +21,7 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            © {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
+            &copy; {{ date('Y') }}, Netgruppen på {{ config('app.name') }}.
         @endcomponent
     @endslot
 @endcomponent
